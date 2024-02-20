06b8027b375cc0129a5b24e1d11a6bd40138fd85
Alecto <fomenkoandrey1978@gmail.com>
2024-02-20 16:49:31 +0200

    замінено завдання та тест 3

--- src/main.ts
+++ src/main.ts
@@ -64,16 +64,16 @@ function createUser(name: string, age: number, isActive: boolean): User {
  * Мета: Створення функції, здатної ідентифікувати статус замовлення і надавати користувачеві зрозуміле пояснення щодо поточного стану замовлення.
  *
  * Вимоги до реалізації:
- * 1. Функція має використовувати enum OrderStatus для визначення можливих статусів замовлення.
- * 2. Використати enum `OrderStatus` зі статусами:
+ * 1. У коді має бути присутній enum OrderStatus з необхідними статусами.
+ * 2. enum OrderStatus повинен мати статуси: 'Pending', 'Shipped', 'Delivered', 'Cancelled'.
+ * 3. Функція має використовувати enum OrderStatus для визначення можливих статусів замовлення.
+ * 4. Функція має приймати один параметр типу OrderStatus і повертати рядок з описом статусу.
+ * 5. Функція повинна правильно обробити кожен статус замовлення, повертаючи відповідне повідомлення:
  * -  'Pending' -> 'Замовлення очікує на обробку',
  * -  'Shipped' -> 'Замовлення було відправлено',
  * -  'Delivered' -> 'Замовлення доставлено',
  * -  'Cancelled' -> 'Замовлення скасовано'
  * -  прокинути помилку з текстом 'Невідомий статус замовлення' в будь-якому іншому випадку.
- * 3. Функція має приймати один параметр типу OrderStatus і повертати рядок з описом статусу.
- * 4. Необхідно правильно обробити кожен статус замовлення, повертаючи відповідне повідомлення.
- * 5. У коді має бути присутній enum OrderStatus з необхідними статусами.
  * 6. Параметри функції та її тип повернення мають бути явно типізовані.
  *
  */
@@ -101,9 +101,9 @@ function getOrderStatus(status: OrderStatus): string {
 }
 
 // Приклад виклику функції
-console.log(getOrderStatus(OrderStatus.Pending))
-console.log(getOrderStatus(OrderStatus.Shipped))
-console.log(getOrderStatus(OrderStatus.Delivered))
-console.log(getOrderStatus(OrderStatus.Cancelled))
+// console.log(getOrderStatus(OrderStatus.Pending))
+// console.log(getOrderStatus(OrderStatus.Shipped))
+// console.log(getOrderStatus(OrderStatus.Delivered))
+// console.log(getOrderStatus(OrderStatus.Cancelled))
 
 export { sumArray, createUser, getOrderStatus, OrderStatus }
